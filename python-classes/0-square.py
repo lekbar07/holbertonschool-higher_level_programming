40e2588a0441dbba05d7d704577219666f538a9c
lekba07 <lekbar@bamis.mr>
2025-05-20 12:28:58 +0000

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    pass
+"""Square generation module for Python project 0x06
+"""
 
+
+class Square:
+    """class defined for square generation
+    """
+    pass
