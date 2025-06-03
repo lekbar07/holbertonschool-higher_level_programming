331443e2b63fc752d52d48c607dda49e11783562
lekba07 <lekbar@bamis.mr>
2025-06-03 09:53:38 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
