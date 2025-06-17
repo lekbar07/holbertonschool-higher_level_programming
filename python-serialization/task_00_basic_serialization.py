3324ebeb91d822c6a86ac91edb358918289457f7
lekba07 <lekbar@bamis.mr>
2025-06-17 12:51:15 +0000

    Add task_00_basic_serialization module with JSON save/load functions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,12 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a Python dictionary and save it to a JSON file."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """Load data from a JSON file and deserialize it into a Python dictionary."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
